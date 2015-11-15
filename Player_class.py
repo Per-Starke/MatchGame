d5565d311fc72801f18683bd4106dd2304f84202
Per Starke <per@starke-team.de>
2015-11-15 18:01:48 +0100

    added name, number, match, bool Victory to class Player

--- Player_class.py
+++ Player_class.py
@@ -1,1 +1,10 @@
 __author__ = 'Per'
+
+class Player(object):
+
+    def __init__(self, name, number, assignedMatch):
+        self.Name = name
+        self.Number = number
+        self.AssignedMatch = assignedMatch
+        self.Victory = None
+        
