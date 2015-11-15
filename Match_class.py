fc39c9193b4da2ff561a20802950ba6b997bd02e
Per Starke <per@starke-team.de>
2015-11-15 17:55:36 +0100

    added class Match_class with number and length + get_functions

--- Match_class.py
+++ Match_class.py
@@ -1,1 +1,14 @@
 __author__ = 'Per'
+
+class Match (object):
+
+    def __init__(self, number, length):
+        self.Number = number
+        self.Length = length
+
+    def getNumber(self):
+        return self.Number
+
+    def getLength(self):
+        return self.Length
+
